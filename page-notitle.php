d9a3ed2533124cf0a7167e84bdbfa4c573926cee
eda <e.ballanca08@gmail.com>
2024-07-26 10:45:17 -0400

    more practice

--- page-notitle.php
+++ page-notitle.php
@@ -1,20 +1,29 @@
-
 <?php 
+	
 /*
-Template Name: page no title 
+	Template Name: Page No Title
 */
-
+	
 get_header(); ?>
 
-   <?php if(have_posts() ):
-      while(have_posts() ): the_post(); ?>
+	<?php 
+	
+	if( have_posts() ):
+		
+		while( have_posts() ): the_post(); ?>
+			
+			<h1>This is my Static Title</h1>
+			
+			<small>Posted on: <?php the_time('F j, Y'); ?> at <?php the_time('g:i a'); ?>, in <?php the_category(); ?></small>
+			
+			<p><?php the_content(); ?></p>
+			
+			<hr>
+		
+		<?php endwhile;
+		
+	endif;
+			
+	?>
 
-     <h1> This Is My Static Title</h1>
-     <small>Posted On: <?php the_time('F j,Y'); ?> at <?php the_time('g:i a'); ?>, in <?php the_category(); ?></small>
-      <p><?php the_content(); ?></p>
-      
-      <hr>
-   <?php endwhile;
-endif;
-   ?>
 <?php get_footer(); ?>
